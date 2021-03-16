1516684fdaa9303486712f3067c3a98f1c3e3332
TitanV <a01098930999@gmail.com>
2021-03-16 23:00:32 +0900

    add - menu

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,8 @@
 import { Component } from 'react';
 import '../style/Main.css';
+import date from '../image/date.svg';
+import trend from '../image/trend.svg';
+import dropdown from '../image/dropdown.svg';
 
 class Main extends Component {
 
@@ -8,9 +11,16 @@ class Main extends Component {
   render() {
     return (
       <div class="main">
-        <div class="container">
-        트렌딩
-
+          <a class="sort" href="/">
+          <img class="sortimg" src={trend} />
+          트렌딩</a>
+          <a class="sort" href="/">
+          <img class="sortimg" src={date} />
+          최신</a>
+        <div>
+          <div class="dropdown">
+            <img  src={dropdown} /> 
+          </div>
         </div>
       </div>
     );
