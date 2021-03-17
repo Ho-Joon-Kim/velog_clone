a14e9a51255c91a0eaa0ff624b918eb803250389
TitanV <a01098930999@gmail.com>
2021-03-17 23:43:20 +0900

    add - i made a dropdown menu

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,24 +5,45 @@ import trend from '../image/trend.svg';
 import dropdown from '../image/dropdown.svg';
 
 class Main extends Component {
+  state = { showMenu: true };
 
+  constructor() {
+    super();
+    
+    this.showMenu = this.showMenu.bind(this);
+  }
+  
+
+  showMenu(event) {
+    event.preventDefault();
+    if(this.state.showMenu){
+      this.setState({ showMenu: false });
+    }else{
+      this.setState({ showMenu: true });
+    }
+  }
 
 
   render() {
     return (
       <div class="main">
-          <a class="sort" href="/">
-          <img class="sortimg" src={trend} />
-          트렌딩</a>
-          <a class="sort" href="/">
-          <img class="sortimg" src={date} />
-          최신</a>
+        <a class="sort" href="/">
+        <img class="sortimg" src={trend} />
+        트렌딩</a>
+        <a class="sort" href="/trend">
+        <img class="sortimg" src={date} />
+        최신</a>
         <div>
-          <div class="dropdown">
-            <img  src={dropdown} /> 
-          </div>
-        </div>
+        <div class="dropdown" onClick={this.showMenu} > <img src={dropdown}/> </div>
+        { this.state.showMenu ? (<div class="dropdownmenu" ref={(element) => { this.dropdownMenu = element;}}>
+          <button> Menu item 1 </button>
+          <button> Menu item 2 </button>
+          <button> Menu item 3 </button>
+          </div>) : ( null )
+        }
       </div>
+        </div>
+
     );
   }
 }
